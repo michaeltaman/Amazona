b64ea636c315c45bbcac3805b3fe434a302d52f4
michaelt <michael.t@superup.me>
2020-09-09 16:23:11 +0300

    Built shopping cart screen (#13 completed)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,18 @@
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
+const cartItems = Cookie.getJSON("cartItems") || [];
 
-const initialState = {};
+const initialState = { cart: { cartItems } };
+
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-})
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
