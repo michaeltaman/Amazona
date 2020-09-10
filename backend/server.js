4ffc987feed41c63e48c9425996a13fce32d91fa
michaelt <michael.t@superup.me>
2020-09-10 11:11:01 +0300

    14. Connection to MongoDB (localhost) established and write data to amazona/users table.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).catch((error) => console.log(error.reason));
 
 const app = express();
 
+app.use('/api/users', userRoute);
 app.get('/api/products/:id', (req, res) => {
   const productId = req.params.id;
   console.log(productId);
   const product = data.products.find((x) => x._id === productId);
   if (product) {
-    //console.log(product);
     res.send(product);
   } else {
-    console.log('xxxx');
     res.status(404).send({ msg: 'Product Not Found.' });
   }
 });
