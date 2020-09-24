8a6a1d20493302e9357529f558a6656842346e8f
michaelt <michael.t@superup.me>
2020-09-24 14:49:15 +0300

    #22. Works properly for me

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,10 +15,11 @@ import {
   PRODUCT_DELETE_FAIL,
 } from '../constants/productConstants';
 
-const listProducts = () => async (dispatch) => {
+const listProducts = (category = '', searchKeyword = '', sortOrder = '') => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get('/api/products');
+    const { data } = await axios.get("/api/products?category=" + category +
+                                      "&searchKeyword=" + searchKeyword + "&sortOrder=" + sortOrder);
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
